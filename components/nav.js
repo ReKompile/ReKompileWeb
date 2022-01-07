2a7953be1ee78181ba8b4f7bc892d8898cb3c674
VolVox99 <alirastegar123@gmail.com>
2022-01-07 13:16:20 -0800

    add new folder

---
+++ components/nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function nav() {
+    return (
+        <nav className="w-full bg-indigo-500 grid grid-cols-1 grid-rows-1">
+            <div>
+                Hey
+            </div>
+        </nav>
+    )
+}
+
+export default nav
