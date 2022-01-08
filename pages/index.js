fbc571a1bd75870894cb782da2bee9d4c2523c4e
VolVox99 <alirastegar123@gmail.com>
2022-01-07 20:23:19 -0800

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,81 @@
 import Image from 'next/image'
 import GradientButton from '../components/GradientButton'
 import OutlineButton from '../components/OutlineButton'
+import Head from 'next/head'
 
 export default function Home() {
 	return (
-		<div className=''>
-			<div className='flex justify-center mt-20 mx-12'>
-				<div className='basis-5/12 mt-10 relative left-14'>
-					<div className='text-8xl'>
-						Teaching kids coding to make a <br />
-						<span className='orange-gradient-text'>
-							{'<'}difference{'>'}
-						</span>
+		<>
+			<Head>
+				<title>
+					ReKompile - Teaching kids coding to make a difference
+				</title>
+				<meta
+					name='title'
+					content='ReKompile - Teaching kids coding to make a difference'
+				/>
+				<meta
+					name='description'
+					content='ReKompile teaches high school students computer science and utilizes their skills to make a change by teaming them up with non profits in need'
+				/>
+
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='http://rekompile.org/' />
+				<meta
+					property='og:title'
+					content='ReKompile - Teaching kids coding to make a difference'
+				/>
+				<meta
+					property='og:description'
+					content='ReKompile teaches high school students computer science and utilizes their skills to make a change by teaming them up with non profits in need'
+				/>
+				<meta property='og:image' content='' />
+
+				<meta property='twitter:card' content='summary_large_image' />
+				<meta property='twitter:url' content='http://rekompile.org/' />
+				<meta
+					property='twitter:title'
+					content='ReKompile - Teaching kids coding to make a difference'
+				/>
+				<meta
+					property='twitter:description'
+					content='ReKompile teaches high school students computer science and utilizes their skills to make a change by teaming them up with non profits in need'
+				/>
+				<meta property='twitter:image' content='' />
+			</Head>
+			<div className=''>
+				<div className='flex justify-center mt-20 mx-12'>
+					<div className='basis-5/12 mt-10 relative left-14'>
+						<div className='text-8xl'>
+							Teaching kids coding to make a <br />
+							<span className='orange-gradient-text'>
+								{'<'}difference{'>'}
+							</span>
+						</div>
+						<div className='flex justify-between max-w-md mt-14'>
+							<OutlineButton>About Us</OutlineButton>
+							<GradientButton>View Classes</GradientButton>
+						</div>
 					</div>
-					<div className='flex justify-between max-w-md mt-14'>
-						<OutlineButton>About Us</OutlineButton>
-						<GradientButton>View Classes</GradientButton>
+					<div className=''>
+						<Image
+							src='/pair_programming.svg'
+							width='1060'
+							height='630'
+							alt='pair programming'
+						/>
 					</div>
 				</div>
 				<div className=''>
 					<Image
-						src='/pair_programming.svg'
-						width='1060'
-						height='630'
-						alt='pair programming'
+						src='/layered-waves.svg'
+						layout='fill'
+						objectFit='contain'
+						objectPosition='bottom'
+						alt='layered waves'
 					/>
 				</div>
 			</div>
-			<div className=''>
-				<Image
-					src='/layered-waves.svg'
-					layout='fill'
-					objectFit='contain'
-					objectPosition='bottom'
-					alt='layered waves'
-				/>
-			</div>
-		</div>
+		</>
 	)
 }
