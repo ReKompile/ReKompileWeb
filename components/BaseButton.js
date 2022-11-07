a36dab61b7489341fe104787d491f57d397ac741
VolVox99 <alirastegar123@gmail.com>
2022-11-06 21:56:10 -0800

    made our work page responsive

--- components/BaseButton.js
+++ components/BaseButton.js
@@ -1,14 +1,16 @@
 import Link from 'next/link'
 
-function BaseButton({ href = null, ...props }) {
+function BaseButton({ href = null, useNextLink = true, ...props }) {
 	let s = `px-5 py-3 md:px-8 md:py-5 lg:px-12 lg:py-5 sm:text-2xl lg:text-2xl font-bai-jamjuree hover:translate-y-1 text-center transition duration-200 ease-in-out ${props.className}`
+
+	const LinkComponent = useNextLink ? Link : 'a'
 
 	if (href) {
 		return (
 			<>
-				<Link href={href} passHref>
+				<LinkComponent href={href} target = '_blank' passHref>
 					<button {...props} className={s} />
-				</Link>
+				</LinkComponent>
 			
 			</>
 		)
--- components/PortfolioBox.js
+++ components/PortfolioBox.js
@@ -4,9 +4,17 @@ export default function PortfolioBox({ title, description, image, link, color}) 
 	return (
 		<div className={`box ${color}`}>
 			<div className='box-data'>
-				<h1>{title}</h1>
-				<p className='box-text'>{description}</p>
-				<WhiteButton href={link}>Visit</WhiteButton>
+				<div>
+					<h1>{title}</h1>
+				</div>
+				<div>
+					<p className='box-text'>{description}</p>
+				</div>
+				<div>
+					<WhiteButton href={link} useNextLink={false}>
+						Visit
+					</WhiteButton>
+				</div>
 			</div>
 			<div className='box-img'>
 				<img className='box-img-img' src={image} />
