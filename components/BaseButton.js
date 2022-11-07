db73d182b5c817406ee8c641a5259b1919021623
okayyyy <zghan2005@gmail.com>
2022-11-06 16:51:55 -0800

    Make home page responsive, adjust text content

--- components/BaseButton.js
+++ components/BaseButton.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 function BaseButton({ href = null, ...props }) {
-	let s = `px-12 py-5 sm:text-2xl lg:text-2xl font-bai-jamjuree hover:translate-y-1 text-center transition duration-200 ease-in-out ${props.className}`
+	let s = `px-5 py-3 md:px-8 md:py-5 lg:px-12 lg:py-5 sm:text-2xl lg:text-2xl font-bai-jamjuree hover:translate-y-1 text-center transition duration-200 ease-in-out ${props.className}`
 
 	if (href) {
 		return (
